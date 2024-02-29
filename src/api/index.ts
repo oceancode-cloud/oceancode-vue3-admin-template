d974fe5b9c21b0414e3f22e0680cdbec4d0dd971
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-02-29 22:38:27 +0800

    update store template

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import { IResultData, useRequest } from '@oceancode/framework';
 const request = useRequest()
 
+/**
+ * ping
+ * @returns ping
+ */
 export function ping(): Promise<IResultData<string>> {
   return request.get().get('/api/ping', {});
 }
