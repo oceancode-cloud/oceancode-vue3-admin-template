04d68fc44675ae3ef66127e8f3f98b6f05e70e83
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-03-03 07:21:28 +0800

    add api mock

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { getStore, setupFramework } from '@oceancode/framework';
+import { getStore, bootstrap } from '@oceancode/framework';
 import '@oceancode/framework/dist/style.css';
 
 import { loadConfig } from './config'
 const app = createApp(App).use(getStore())
-setupFramework(app, loadConfig())
+bootstrap(app, loadConfig())
   .then(() => {
     app.mount('#app')
   })
