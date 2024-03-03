6e6ede4dc4d0f6866b4d09d1b4f5341317a5912b
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-03-03 19:31:31 +0800

    delete pageEnum

--- src/models/common/enum/index.ts
+++ src/models/common/enum/index.ts
@@ -1,2 +1,1 @@
 export * from './httpEnum'
-export * from './pageEnum'
--- src/models/common/enum/pageEnum.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @description 页面枚举
- */
-export enum PageEnum {
-  LOGIN_ROUTE_NAME = 'login',
-  DASHBOARD_ROUTE_NAME = 'home'
-}
