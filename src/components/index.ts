b13ca4218d49dcef29c887206ac5771c790706d4
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-07-14 15:01:01 +0800

    add router context

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import AppContext from "./common/AppContext.vue"
+import AppRouterContext from "./common/AppRouterContext.vue"
 import AppRouter from './common/AppRouter.vue'
 
 export {
   AppContext,
+  AppRouterContext,
   AppRouter
 }
