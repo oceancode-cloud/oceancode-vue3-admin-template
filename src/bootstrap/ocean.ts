b114e019531f2ec31ce6cdf727372727cc266a69
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-07-14 10:36:42 +0800

    register components

--- src/bootstrap/ocean.ts
+++ src/bootstrap/ocean.ts
@@ -6,9 +6,13 @@ import {
   OPopconfirm,
   OButton,
   OSpace,
+  OUsernameInput,
+  OPasswordInput,
 } from '@oceancode/ocean-wui'
 
 export function setupOcean(app:App){
+  app.component("OPasswordInput",OPasswordInput)
+  app.component("OUsernameInput",OUsernameInput)
   app.component("OAvatar",OAvatar)
   app.component("OInput",OInput)
   app.component("OIcon",OIcon)
