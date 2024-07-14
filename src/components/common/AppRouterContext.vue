c0786bf36b01e7b73e51448b501186196d8c26af
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-07-14 15:01:01 +0800

    add router context

---
+++ src/components/common/AppRouterContext.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view v-slot="{Component}" v-if="$route.meta && $route.meta.keepAlive">
+    <keep-alive>
+      <o-context>
+        <component :is="Component"></component>
+      </o-context>
+    </keep-alive>
+  </router-view>
+  <router-view v-else></router-view>
+</template>
+<script lang="ts">
+export default {
+  name:'AppRouterContext'
+}
+</script>
+<script lang="ts" setup>
+</script>
