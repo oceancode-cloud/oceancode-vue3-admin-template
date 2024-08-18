2f424f0de087902397c55dd9d79961349923a9ff
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-08-18 10:54:20 +0800

    fix bug

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -14,94 +14,10 @@ const routeList = [
   },
   {
     id:2,
-    path: '/home',
     parentId: -2,
-    name:'home',
-    component: () => import('@/views/home/Home.vue')
-  },
-  {
-    id:3,
-    path: '/dashboard',
-    component: () => import('@/packages/layout/common/CommonLayout.vue')
-  },
-  {
-    id: 333,
-    component: () => import('@/packages/layout/common/CommonLayout.vue')
-  },
-  {
-    id:4,
-    path: '/login',
-    name:'login',
-    parentId:-2,
-    meta:{
-      permissions:['unlogin']
-    },
-    component: () => import('@/views/user/Userlogin.vue')
-  },
-  {
-    id:5,
-    parentId:3,
-    path: '/user',
-    name:'userList',
-    component: () => import('@/views/user/UserList.vue')
-  },
-  {
-    id:5,
-    parentId:3,
-    path: '/data/dict',
-    name:'dataDict',
-    component: () => import('@/views/data/DictManager.vue')
-  },
-  {
-    id: 349,
-    path: '/menu/list',
-    name: 'menuList',
-    parentId: 3,
-    meta:{
-      permissions: ['login', ],
-    },
-    component: () => import('@/views/system/MenuList.vue')
-  },
-  {
-    id: 351,
-    path: '/task/manager',
-    name: 'taskManager',
-    parentId: 333,
-    meta:{
-      permissions: ['login', ],
-    },
-    component: () => import('@/views/system/TaskManager.vue')
-  },
-  {
-    id: 352,
-    path: '/dashoard',
-    name: 'dashoard',
-    parentId: 333,
-    meta:{
-      permissions: ['login', ],
-    },
-    component: () => import('@/views/home/Dashoard.vue')
-  },
-  {
-    id: 357,
-    path: '/role/list',
-    name: 'roleList',
-    parentId: 333,
-    meta:{
-      permissions: ['login', ],
-    },
-    component: () => import('@/views/system/RoleList.vue')
-  },
-  {
-    id: 360,
-    path: '/param/list',
-    name: 'paramList',
-    parentId: 333,
-    meta:{
-      permissions: ['login', ],
-    },
-    component: () => import('@/views/system/ParamList.vue')
-  },
+    path:'/home',
+    component: () => import('@/views/home.vue')
+  }
 ]
 
 export const routes = deepTree(routeList)
