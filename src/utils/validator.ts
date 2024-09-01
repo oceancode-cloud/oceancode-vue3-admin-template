f583c4059dc9a2d8cbc922812aa116ab3bf6d8f6
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-09-01 21:55:07 +0800

    update

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -8,3 +8,16 @@ export function validatorWithArtifactId(required:boolean,message: string,value:a
       return new Error("只能包含小写字母，数字，中划线，且以字母开头");
   }
 }
+
+
+export function validatorWithIp(required:boolean,message: string,value:any) {
+  const pattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(?!$)|$)){4}$/
+  if(required){
+    if(!value && value!=0 && value!=false){
+      return new Error(message)
+    }
+  }
+  if(!pattern.test(value)){
+    return new Error('IP地址非法')
+  }
+}
