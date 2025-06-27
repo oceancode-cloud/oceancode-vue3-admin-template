80869e839d72adb847812dca9650faf08b1cfb53
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-06-27 21:38:08 +0800

    add Custom Component Category

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,8 @@ import AppRouterContext from "./common/AppRouterContext.vue"
 import AppRouter from './common/AppRouter.vue'
 import MessageContent from './common/MessageContent.vue'
 
+export * from './custom'
+
 export {
   AppContext,
   AppRouterContext,
