ece1916237de1c89092d974c959221c1233d7a47
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-08 16:01:43 +0800

    update

--- src/meta/pages/app/index.ts
+++
@@ -1,9 +0,0 @@
-export namespace AppList{
-   export const ID = 'app-list'
-    export const ROUTER_NAME = 'appList'
-}
-
-export namespace AccountList {
-    export const ID = 'account-list'
-    export const ROUTER_NAME = 'accountList'
-}
