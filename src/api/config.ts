97b45a01412db1fd802d96c97a988d33198b0704
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-08 18:29:39 +0800

    remove api

--- src/api/config.ts
+++
@@ -1,8 +0,0 @@
-import { ResultData, useRequest, apiResponseWrapper, DataHandle, resolveQueryResult } from '@oceancode/ocean-wui'
-export const request_ = useRequest()
-export const API_PREFIX_ = import.meta.env.VITE_GLOB_API_URL || ''
-const QUERY_API_PREFIX_ = API_PREFIX_ + (import.meta.env.VITE_GLOB_API_PATH || '/graphql/query')
-
-export function queryWithDsl<T, R = T>(querys: object, cb?: DataHandle<T, R>): ResultData<R> {
-  return apiResponseWrapper(request_.dsl(QUERY_API_PREFIX_, querys), (res: any) => resolveQueryResult(res, cb))
-}
--- src/api/method.ts
+++
@@ -1,6 +0,0 @@
-import { buildDsl } from '@oceancode/ocean-wui'
-
-export function currentUser(fields?: Array<string | object>): string {
-  return buildDsl('current_user',[],fields || ['avatar', 'nickname', 'userInfo', 'username', ])
-}
-
