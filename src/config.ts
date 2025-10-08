52e7e0f5b36968dca819d35f651402ec5f63e07e
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-08 18:28:13 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,8 @@
+import { setConfig } from 'ocean-admin-api/index';
 const API_PREFIX_ = import.meta.env.VITE_GLOB_API_URL || ''
 const QUERY_API_PREFIX_ = API_PREFIX_ + (import.meta.env.VITE_GLOB_API_PATH || '/graphql/query')
+
+setConfig({
+  baseUrl: API_PREFIX_,
+  queryUrl: QUERY_API_PREFIX_
+})
