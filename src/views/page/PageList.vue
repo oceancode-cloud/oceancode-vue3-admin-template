a3a035b05bf64f67ba8a18c23c4c06e3f3944d07
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-26 01:44:55 +0800

    update

---
+++ src/views/page/PageList.vue
@@ -0,0 +1,125 @@
+<!-- Copyright (C) Ocean Code Cloud. 2025-2025 .All Rights Reserved. -->
+
+<template>
+  <div
+    >
+    <div
+      :style="{
+        'z-index': 0,
+        top: '21.0px',
+        left: '24.0px',
+        width: '178px',
+        position: 'absolute',
+        height: '32px',
+      }"
+    >
+      <o-text
+        :font-weight="600"
+        color="#000"
+        text-align="left"
+        type="text"
+      font-size="20px"
+      :text-style="{
+        fontSize:'20px',
+      }"
+      >
+        页面列表
+      </o-text>
+    </div>
+    <div
+      :style="{
+        'z-index': 2,
+        top: '78.0px',
+        left: '24.0px',
+        width: '300px',
+        position: 'absolute',
+        height: '91.33333333333333%',
+      }"
+    >
+      <o-group-tree
+        title="页面分组"
+        key-field="id"
+        label-field="name"
+        :default-expand-all="true"
+        :default-expanded-keys="[]"
+        :data="listPageGroupsTree"
+        :on-item-click='handleItemClick1725369886925'
+      >
+      </o-group-tree>
+    </div>
+    <div
+      :style="{
+        'z-index': 3,
+        top: '78.0px',
+        left: '330.0px',
+        width: '75.13888888888889%',
+        position: 'absolute',
+        height: '91.33333333333333%',
+      }"
+    >
+      <div
+      >
+        <o-data-table ref="Table1725369927476" ></o-data-table>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useDataTable } from '@oceancode/ocean-wui';
+import { listPageGroupsTree } from '@common-api/api/page/PageFunction';
+
+const Table1725369927476 = useDataTable({
+  columns: [
+    {
+      title:'名称',
+      key:'name',
+    },
+    {
+      title:'唯一标识',
+      key:'pageId',
+    },
+    {
+      title:'类型',
+      key:'type',
+    },
+    {
+      title:'实现方式',
+      key:'implType',
+    },
+    {
+      title:'描述',
+      key:'description',
+    },
+    {
+      title: '操作',
+      type: 'action',
+      fixed:'right',
+      actions: [
+        {
+          type: 'add',
+          text: '新增',
+        },
+        {
+          type: 'edit',
+          text: '编辑',
+        },
+        {
+          type: 'delete',
+          text: '删除',
+          onClick(param, option, ctx) {
+          },
+        },
+      ],
+    },
+  ],
+  on: {
+    load(param: any) {
+      return [];
+    }
+  },
+});
+async function handleItemClick1725369886925(param): Promise<void> {
+  Table1725369927476.value.refresh(param);
+}
+
+</script>
