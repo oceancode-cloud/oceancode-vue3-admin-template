6d699d59cd4625bc71f0d5b7325f0d061d5e6d2d
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-25 23:17:20 +0800

    update

---
+++ src/components/custom/model/model-link-name.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="model-name" @click="handleClick">
+    {{value.name}}({{ value.modelId }})
+  </span>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+import { useRouter } from '@oceancode/ocean-wui'
+const props = defineProps({
+  value: Object
+})
+const router = useRouter()
+function handleClick(e){
+  e.preventDefault();
+  e.stopPropagation();
+  router.open({name:'modelManager',params:{modelId:props.value.id}})
+}
+</script>
+<style lang="scss" scoped>
+.model-name{
+  color:blue;
+}
+.model-name:hover{
+  font-style: italic;
+}
+</style>
