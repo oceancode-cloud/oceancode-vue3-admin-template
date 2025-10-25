2f1903388dc76e953ef16d90625578aabaca0a95
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-26 02:38:30 +0800

    update

--- src/views/page/page-list/Contextmenu.ts
+++ src/views/page/page-list/Contextmenu.ts
@@ -1,16 +1,40 @@
 /**
  * Copyright (C) Ocean Code Cloud. 2025-2025 .All Rights Reserved.
  */
+import { deletePageGroupById } from '@common-api/api/page/PageFunction';
 
 export function loadContextMenu(param: any) {
   return [
     {
       label:'新增分组',
       key: '1761414842915_0',
+      eventType: 'open',
+      dialog: {
+        id: 'add-group-form',
+        value: {
+          parentId: param?.id,
+        },
+      },
     },
     {
       label:'修改分组',
       key: '1761414842915_1',
+      eventType: 'open',
+      dialog: {
+        id: 'update-group-form',
+        value: {
+          id: param?.id,
+          name: param?.name,
+          code: param?.code,
+        },
+      },
+    },
+    {
+      label:'删除分组',
+      key: '1761414842915_2',
+       onClick(param: any) {
+        return deletePageGroupById(param?.id);
+      }
     },
   ]
 }
