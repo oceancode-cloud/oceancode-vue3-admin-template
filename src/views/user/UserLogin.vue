93c018ee69038f4b22cad778965533c91b766d70
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-26 16:04:08 +0800

    update

--- src/views/user/UserLogin.vue
+++ src/views/user/UserLogin.vue
@@ -138,6 +138,7 @@ import { UserLoginRequest } from '@common-api/models/user/UserLoginRequest';
 import { useForm, OUsernameInput, OPasswordInput, useUser, useRouter } from '@oceancode/ocean-wui';
 import { WorkspaceList } from '@common-api/meta/pages/workspace/index';
 import { UserRegister, ResetPassword } from '@common-api/meta/pages/user/index';
+import { toRedirect } from '@/utils';
 
 const user = useUser();
 const router = useRouter();
@@ -189,6 +190,9 @@ const Form = useForm({
       const res = userLogin(param);
       res.then(async (data) => {
         user.setLoginResponse(data);
+        if(toRedirect()){
+          return;
+        }
         await user.refreshPermission();
         router.push({ name: WorkspaceList.ROUTER_NAME });
       });
