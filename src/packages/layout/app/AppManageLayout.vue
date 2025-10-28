36d3b18079ca19c66e74d9266af8659a41c38860
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-28 22:49:13 +0800

    update

--- src/packages/layout/app/AppManageLayout.vue
+++ src/packages/layout/app/AppManageLayout.vue
@@ -20,6 +20,22 @@
             borderTopLeftRadius: '0px',
           }"
       >
+        <div
+          :style="{
+            'z-index': 2,
+            top: '148.0px',
+            left: '3.0px',
+            width: '278px',
+            position: 'absolute',
+            height: '38px',
+          }"
+        >
+          <o-segment-tabs
+            :options="loadOptions1761662783778"
+            :on-item-click="handleTabClick1761662783778"
+          >
+          </o-segment-tabs>
+        </div>
       </div>
       <div
         :style="{
@@ -40,5 +56,27 @@
 </template>
 <script lang="ts" setup>
 import CommonLayout from '@/packages/layout/common/CommonLayout.vue';
+import { useRouter } from '@oceancode/ocean-wui';
+
+const router = useRouter();
+const loadOptions1761662783778 = [
+  {
+    label:'代码',
+    key:'key0',
+  },
+  {
+    label:'依赖管理',
+    key:'key1',
+  },
+  {
+    label:'设置',
+    key:'key2',
+  },
+]
+async function handleTabClick1761662783778(item): Promise<void> {
+  if (item.router) {
+    router.push(item.router);
+  }
+}
 
 </script>
