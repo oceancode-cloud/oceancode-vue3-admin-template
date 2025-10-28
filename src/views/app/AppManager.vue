382ad15085df3ecb04af8a54ffef4c6b056d1375
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-28 22:41:15 +0800

    update

--- src/views/app/AppManager.vue
+++ src/views/app/AppManager.vue
@@ -3,6 +3,24 @@
 <template>
   <div
     >
+    <div
+        :style="{
+          border: '0px solid #000',
+          borderTopLeftRadius: '0px',
+          'z-index': 0,
+          transform: 'rotate(0deg)',
+          top: '0.0px',
+          left: '0.0px',
+          borderBottomLeftRadius: '0px',
+          background: 'rgba(255, 255, 255, 1)',
+          width: '100%',
+          borderBottomRightRadius: '0px',
+          borderTopRightRadius: '0px',
+          position: 'absolute',
+          height: '173px',
+        }"
+    >
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
