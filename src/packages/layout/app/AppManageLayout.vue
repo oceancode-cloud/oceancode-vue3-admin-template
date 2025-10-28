3ae49d815da6d51e4d3a7b33418fb85a798ee5e0
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-10-28 22:44:36 +0800

    update

--- src/packages/layout/app/AppManageLayout.vue
+++ src/packages/layout/app/AppManageLayout.vue
@@ -4,7 +4,37 @@
   <div
     >
     <CommonLayout :show-menu="false">
-      <app-router-context></app-router-context>
+      <div
+          :style="{
+            'z-index': 0,
+            transform: 'rotate(0deg)',
+            top: '0.0px',
+            left: '0.0px',
+            borderBottomLeftRadius: '0px',
+            background: 'rgba(255, 255, 255, 1)',
+            width: '100%',
+            borderBottomRightRadius: '0px',
+            borderTopRightRadius: '0px',
+            position: 'absolute',
+            height: '190px',
+            borderTopLeftRadius: '0px',
+          }"
+      >
+      </div>
+      <div
+        :style="{
+          'z-index': 1,
+          top: '198.0px',
+          left: '0.0px',
+          width: '100%',
+          position: 'absolute',
+          height: '698px',
+        }"
+      >
+        <app-router-context
+        >
+        </app-router-context>
+      </div>
     </CommonLayout>
   </div>
 </template>
