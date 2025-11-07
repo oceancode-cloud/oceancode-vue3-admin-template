506efc6419308a446f6a22a79f8e62ce8402ee18
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-11-07 20:46:11 +0800

    update

---
+++ src/views/app/AppSettingFormPanel.vue
@@ -0,0 +1,35 @@
+<!-- Copyright (C) Ocean Code Cloud. 2025-2025 .All Rights Reserved. -->
+
+<template>
+  <div
+    >
+    <div
+      :style="{
+        'z-index': 0,
+        top: '0.0px',
+        left: '0.0px',
+        width: '229px',
+        position: 'absolute',
+        height: '29px',
+      }"
+    >
+      <o-text
+        :font-weight="600"
+        color="#000"
+        text-align="left"
+        type="text"
+      font-size="18px"
+      :text-style="{
+        fontSize:'18px',
+      }"
+      >
+        {{ global.currentMenu.name }}
+      </o-text>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useGlobal } from '@/store';
+
+const global = useGlobal();
+</script>
